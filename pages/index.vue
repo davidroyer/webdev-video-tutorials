564c55a8f9128d1804a39b81fa8704466955c654
David Royer <droyer01@gmail.com>
2019-08-14 22:54:57 -0400

    chore: update

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,17 @@
         Nuxt Demo
       </h1>
       <h2>Vimeo</h2>
+
+      <div style="padding:56.25% 0 0 0;position:relative;">
+        <iframe
+          src="https://vimeo.com/showcase/2957711/embed"
+          allowfullscreen
+          frameborder="0"
+          style="position:absolute;top:0;left:0;width:100%;height:100%;"
+        ></iframe>
+      </div>
+
+      <hr class="vid-seperator" />
       <iframe
         src="https://player.vimeo.com/video/101059360"
         width="640"
